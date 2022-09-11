e6641f16dd307282397a570500e5f7f6e802a046
OSERHIRDEV <oserhir10@gmail.com>
2022-09-11 14:56:35 +0100

    Set Cookie Parser, sessions and flash

--- Database/app.js
+++ Database/app.js
@@ -1,8 +1,15 @@
+// Require the NPM packages that we need
 const mongoose = require("mongoose");
 const express = require("express");
-const app = express();
-const db = require("./config/database"); // connect to Database
 const bodyParser = require("body-parser");
+const db = require("./config/database"); // Connect to Database
+
+// Initalise a new express application
+const app = express();
+// Flash
+const session = require("express-session");
+const flash = require("connect-flash");
+const cookieParser = require("cookie-parser");
 
 // Static File
 app.use(express.static("images"));
@@ -11,12 +18,12 @@ app.use("/style", express.static(__dirname + "public/style"));
 app.use("/images", express.static(__dirname + "public/images"));
 app.use("/uploads", express.static(__dirname + "public/uploads"));
 
-// Template Engine
+// Setting our view engine as EJS
 const ejs = require("ejs");
 app.set("view engine", "ejs");
 
 // Middleware
-// app.use(express.json());
+// This allows us to pass data from the form
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -24,19 +31,25 @@ app.use(
   })
 );
 
+// Set Cookie Parser, sessions and flash
+app.use(cookieParser("SecritStringForCookies")); // Create a new cookie parser middleware
+app.use(
+  session({
+    secret: "SecritStringForSession",
+    cookie: { maxAge: 60000 }, // A positive value indicates when the cookie should expire relative to the current time
+    resave: true, // Forces the session to be saved back to the session store,
+    saveUninitialized: true, // Forces a session that is "uninitialized" to be saved to the store
+  })
+);
+app.use(flash());
+
 // Routes
 const blogRouter = require("./Router/blog");
 app.use("/", blogRouter);
 
-// Listen on port 3000
-
+// Set a default environment port or cutom port - 3000
 const port = process.env.PORT || 3000;
+// Start out application
 app.listen(port, () => {
   console.log(`Server is running on port ${port} `);
 });
-
-/*
-// connect to database => /config/database
-// Creat Schema => /models/blogSchema
-// Create New Post  /seed/post-seed  ( node post-seed.js )
-*/
